8b9c0fb6b153652bc6ed112e3b22f61db7d0f01a
marklitinskiy19 <marklitinskij6@gmail.com>
2026-01-26 11:07:57 +0300

    Create utils.h

---
+++ utils.h
@@ -0,0 +1,9 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include "database.h"
+
+// Показать фильмы после 18:00 и длительностью > 1ч 40мин
+void show_late_long_films(FilmSession* arr, int size);
+
+#endif
